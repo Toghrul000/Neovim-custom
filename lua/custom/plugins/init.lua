6b0683657fd9b6700d92f816d40de8413e7d47d2
s2428245 <t.garalov@student.utwente.nl>
2023-10-03 11:38:18 +0200

    first commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,56 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+local function my_on_attach(bufnr)
+	local api = require "nvim-tree.api"
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+	vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+
+
+return {
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup {
+				on_attach = my_on_attach,
+
+			}
+		end,
+	},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
